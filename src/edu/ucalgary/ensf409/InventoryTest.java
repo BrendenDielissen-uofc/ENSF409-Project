e5f9a74c2b5ba2b6e587c80063bd24c5379138a8
Nafisa <nafisa.tabassum@ucalgary.ca>
2021-04-03 12:15:31 -0600

    code stubs for unit tests

---
+++ src/edu/ucalgary/ensf409/InventoryTest.java
@@ -0,0 +1,22 @@
+package edu.ucalgary.ensf409;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class InventoryTest {
+
+    public InventoryTest() {
+
+    }
+
+    @Test
+    public void testDbConnection() {
+
+    }
+
+    @Test
+    public void testDbCloseConnection() {
+
+    }
+
+}
